8a3b22cc159951360882b4db3b4a965f9b7efc22
grimmauld <soeren@benjos.de>
2021-04-16 16:44:06 +0200

    Pause client ticking during ponder rendering

---
+++ src/main/java/de/oshgnacknak/create_ponder_wonder/mixin/PauseClientMixin.java
@@ -0,0 +1,18 @@
+package de.oshgnacknak.create_ponder_wonder.mixin;
+
+import de.oshgnacknak.create_ponder_wonder.CreatePonderWonder;
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Minecraft.class)
+public class PauseClientMixin {
+
+	@Inject(at = @At(value = "INVOKE"), method = "Lnet/minecraft/client/Minecraft;runTick()V", cancellable = true)
+	private void tick(CallbackInfo ci) {
+		if (CreatePonderWonder.PONDER_RENDERER.isRendering())
+			ci.cancel();
+	}
+}
